49c069e90010d3bc33b745eabb53d77a338737f8
Janikus <64164163+Janikus@users.noreply.github.com>
2020-05-23 11:54:22 +0200

    Delete 3. cifrado cesar.py

--- 3. cifrado cesar.py
+++
@@ -1,9 +0,0 @@
-k = int(input("Introduce value for k: "))
-
-plain = input("Introduce a plain text: ")
-cipher = ""
-
-for i in range(len(plain)):
-    cipher += chr(((ord(plain[i]) + k - 65) % 26) + 65)
-
-print(cipher)
